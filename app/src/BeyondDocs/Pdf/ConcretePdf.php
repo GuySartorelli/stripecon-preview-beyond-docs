581509f8ca119f4e58e552169b2303005b95b6d7
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-09-01 17:04:51 +1200

    Mark concrete PDF class as incomplete

--- app/src/BeyondDocs/Pdf/ConcretePdf.php
+++ app/src/BeyondDocs/Pdf/ConcretePdf.php
@@ -3,9 +3,19 @@
 namespace App\BeyondDocs\Pdf;
 
 use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\ORM\CMSPreviewable;
 use SilverStripe\ORM\DataObject;
 
+/**
+ * Note: This example is not complete - it's just a skeleton to show how a concrete PDF DataObject might related to the PdfTemplates.
+ * You would include some javascript so that when the template is selected, it updates the DataItemID and NameField dropdowns.
+ * It would also update the Content field with the content from the template.
+ * You could then edit the content if the template wasn't 100% exactly what you wanted for this situation.
+ * An onAfterWrite() would save or print or email the resultant PDF, and set the CMS fields to readonly. At that stage it will have
+ * been sent off the the client and exists in the DB for recording purposes only.
+ * Probably you'd want it to be versioned, and have that on onAfterPublish() instead.... you get the idea.
+ */
 class ConcretePdf extends DataObject implements CMSPreviewable
 {
     use Pdf, PdfPreviewable;
@@ -44,6 +54,8 @@ class ConcretePdf extends DataObject implements CMSPreviewable
             DropdownField::create('DataItemID', $this->fieldLabel('DataItem'), [])
                 ->setDescription('The data item which will be used to substitute variables for the PDF.')
         );
+
+        $fields->insertBefore('TemplateID', LiteralField::create('notice', '<p class="alert alert-danger">This is not fully implemented - see comment in code.</p>'));
 
         return $fields;
     }
