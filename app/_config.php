2b45ff146738bc242e0185901c856b62924c8041
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-09-07 16:33:57 +1200

    Hide admins we don't need

--- app/_config.php
+++ app/_config.php
@@ -1,9 +1,29 @@
 <?php
 
+use SilverStripe\Admin\CMSMenu;
+use SilverStripe\Admin\SecurityAdmin;
+use SilverStripe\AssetAdmin\Controller\AssetAdmin;
+use SilverStripe\CampaignAdmin\CampaignAdmin;
+use SilverStripe\Reports\ReportAdmin;
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
+use SilverStripe\SiteConfig\SiteConfigLeftAndMain;
+use SilverStripe\VersionedAdmin\ArchiveAdmin;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+
+// Remove all the admin sections I don't care about
+$removeAdmins = [
+    ArchiveAdmin::class,
+    AssetAdmin::class,
+    CampaignAdmin::class,
+    ReportAdmin::class,
+    SecurityAdmin::class,
+    SiteConfigLeftAndMain::class,
+];
+foreach ($removeAdmins as $class) {
+    CMSMenu::remove_menu_class($class);
+}
