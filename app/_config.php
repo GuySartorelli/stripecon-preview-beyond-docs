307a279e2ec6e0f0b0e4579fb99c18ee7b846d51
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-09-07 15:22:54 +1200

    Remove commented out debug stuff

--- app/_config.php
+++ app/_config.php
@@ -7,12 +7,3 @@ use SilverStripe\Security\Member;
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
-
-// $c = '[image src="/assets/93ba4c36d0/success.png" id="1" width="128" height="128" class="leftAlone ss-htmleditorfield-file image"][';
-
-// preg_match_all('/\[image\s+[^\]]*?id="(?<id>\d+)".*?\]/i', $c, $matches);
-// echo '<pre>';
-// preg_replace_callback('/\[image\s+[^\]]*?id="(?<id>\d+)".*?\]/i', function($matches) {
-//     var_dump($matches);
-// }, $c);
-// echo '</pre>';
