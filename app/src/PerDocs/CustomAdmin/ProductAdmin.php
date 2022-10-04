4c8b7738a06ab335a0796f7c59fd4969a0af860b
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-10-05 12:03:10 +1300

    Account for CMS requirements

--- app/src/PerDocs/CustomAdmin/ProductAdmin.php
+++ app/src/PerDocs/CustomAdmin/ProductAdmin.php
@@ -4,6 +4,7 @@ namespace App\PerDocs\CustomAdmin;
 
 use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\View\Requirements;
 use SilverStripe\View\SSViewer;
 
 class ProductAdmin extends ModelAdmin
@@ -35,13 +36,19 @@ class ProductAdmin extends ModelAdmin
             $this->httpError(404);
         }
 
-        // Allow use of front-end themes temporarily.
+        // Include use of a front-end theme temporarily and clear any CMS requirements.
         $oldThemes = SSViewer::get_themes();
         SSViewer::set_themes(SSViewer::config()->get('themes'));
+        Requirements::clear();
+
+        // Render the preview content
+        // Note that if your template is an include and relies on global css or js, you should
+        // use the Requirements API here to include those
         $preview = $obj->forTemplate();
 
-        // Make sure to set back to backend themes.
+        // Make sure to set back to backend themes and restore CMS requirements.
         SSViewer::set_themes($oldThemes);
+        Requirements::restore();
 
         return $preview;
     }
